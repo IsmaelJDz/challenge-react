4a5a1ad6e3ddf5ca5421b606c091912cd3186799
Ismael Barrios R <ismael_br7@hotmail.com>
2021-05-26 16:09:40 -0500

    [Updated] url ssr and readme.md

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home(props) {
  */
 
 export async function getStaticProps(context) {
-  const res = await fetch("https://challenge-react.vercel.app//api/data");
+  const res = await fetch("https://challenge-react.vercel.app/api/data");
   const data = await res.json();
 
   if (!data) {
